70af0102f97cc4aa84c859628b903f1da2764f4b
aristidei <a.isingizwe@alustudent.com>
2024-02-06 07:03:55 +0200

    typeorm setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieSession from 'cookie-session';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(
+    cookieSession({
+      keys: ['virtulearn'],
+    }),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: false,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Virtulearn Swagger')
+    .setDescription('Full Documentation  of Virtulearn backend using NestJs')
+    .setVersion('1.0')
+    .addTag('Routes')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
