d502c408fa998e8b72ea3a3507a52463c18ba7f3
aristidei <a.isingizwe@alustudent.com>
2024-03-12 19:19:48 +0200

    project init

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authRouter from "./routes/auth.routes.js";
+
+// config Consts
+const app = express();
+
+// Config
+dotenv.config();
+app.use(cookieParser());
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("sample");
+  return res.status(200).json({ done: true });
+});
+
+app.use("/auth", authRouter);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+  connectDB();
+});
