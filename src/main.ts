c39d0b920e8fa448bed182b9c00ce4b4c7bfd894
aristidei <a.isingizwe@alustudent.com>
2024-03-12 16:40:12 +0200

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.use(
-    cookieSession({
-      keys: ['virtulearn'],
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: false,
-    }),
-  );
-
-  const config = new DocumentBuilder()
-    .setTitle('Virtulearn Swagger')
-    .setDescription('Full Documentation  of Virtulearn backend using NestJs')
-    .setVersion('1.0')
-    .addTag('Routes')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   await app.listen(3000);
 }
 bootstrap();
