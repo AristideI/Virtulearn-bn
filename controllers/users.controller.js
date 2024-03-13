0b0d4767e10d2517fdeb5945d9faca1b450181cf
aristidei <a.isingizwe@alustudent.com>
2024-03-13 17:04:05 +0200

    validate token guard

---
+++ controllers/users.controller.js
@@ -0,0 +1,15 @@
+export function getAllUsers(req, res) {
+  res.json({ message: "Welcome to the user route" });
+}
+
+export function getAllStudents(req, res) {
+  res.json({ message: "Welcome to the user route" });
+}
+
+export default function getAllMentors(req, res) {
+  res.json({ message: "Welcome to the user route" });
+}
+
+export function getOneUser(req, res) {
+  res.json({ message: "Welcome to the user route" });
+}
