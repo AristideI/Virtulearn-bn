2f0ead6763728f13d478ea737c68662b6eec8e0b
umutesimagnificat@gmail.com <umutesimagnificat@gmail.com>
2024-03-14 21:12:02 +0200

    feat: content model endpoints

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { connectDB } from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
-
+import contentRouter from "./routes/content.routes.js";
 // config Consts
 const app = express();
 
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/auth", authRouter);
-
+app.use("/content", contentRouter);
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
